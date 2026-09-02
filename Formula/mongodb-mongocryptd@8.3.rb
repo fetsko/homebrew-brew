99cd718eee2908803c236676c525d30355e56e73
Evan Fetsko <emfetsko@gmail.com>
2026-09-02 11:35:22 -0400

    Update Homebrew formulae for MongoDB 9.0.0

--- Formula/mongodb-mongocryptd@8.3.rb
+++ Formula/mongodb-mongocryptd@8.3.rb
@@ -3,11 +3,11 @@ class MongodbMongocryptd < Formula
   homepage "https://www.mongodb.com/"
 
   if Hardware::CPU.intel?
-    url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-x86_64-enterprise-9.0.0.tgz"
-    sha256 "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+    url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-x86_64-enterprise-8.3.7.tgz"
+    sha256 "98294749fd235f318751fb3d9dc76b69b3bf57bf66b93a07f76e560b4e75f485"
   else
-    url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-arm64-enterprise-9.0.0.tgz"
-    sha256 "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+    url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-arm64-enterprise-8.3.7.tgz"
+    sha256 "1a99fa8c41ddb5fbe61bae41f81ff46d32c9c5c0e86a9a54a3b8841d012b1f38"
   end
   license "MongoDB Customer Agreement"
 
